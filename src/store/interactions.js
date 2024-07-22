af2143b124f4170823eabc7c5ca1dbaa1ae07eeb
alricf <alricfernandes11@gmail.com>
2024-07-22 11:49:55 -0400

    Adds remove liquidity form

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -18,6 +18,9 @@ import {
   depositRequest,
   depositSuccess,
   depositFail,
+  withdrawRequest,
+  withdrawSuccess,
+  withdrawFail,
   swapRequest,
   swapSuccess,
   swapFail
@@ -107,6 +110,22 @@ export const addLiquidity = async (provider, amm, tokens, amounts, dispatch) => 
   }
 };
 
+// ----------------------------------------------------------
+// REMOVE LIQUIDITY
+export const removeLiquidity = async (provider, amm, shares, dispatch) => {
+  try {
+    dispatch(withdrawRequest());
+
+    const signer = await provider.getSigner();
+
+    let transaction = await amm.connect(signer).removeLiquidity(shares);
+    await transaction.wait();
+
+    dispatch(withdrawSuccess(transaction.hash));
+  } catch (error) {
+    dispatch(withdrawFail());
+  }
+};
 
 // ----------------------------------------------------------
 // SWAP
