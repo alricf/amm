1af282093e5a03326ff7176de4b99cc4f6b2b0a3
alricf <alricfernandes11@gmail.com>
2024-08-22 10:49:08 -0400

    Updating styling

--- src/components/Withdraw.js
+++ src/components/Withdraw.js
@@ -56,7 +56,7 @@ const Withdraw = () => {
 
   return (
     <div>
-      <Card style={{ maxWidth: '450px' }} className='mx-auto px-4'>
+      <Card style={{ maxWidth: '450px', backgroundColor: '#F86EE7' }} className='mx-auto px-4'>
         {account ? (
           <Form onSubmit={withdrawHandler} style={{ maxWidth: '450px', margin: '50px auto' }}>
 
